f5dd3b8fb0df48926d8bd18e24992a04d300a982
ruheni <aleruheni@outlook.com>
2020-05-03 11:29:53 +0300

    changed postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // paths to all templates in project
-    content: [
-        './pages/**/*.tsx',
-        './components/**/*tsx'
-    ],
-    // any special characters
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
-
 module.exports = {
-    plugins: [
-        "postcss-import",
-        "autoprefixer",
-        ...(process.env.NODE_ENV === 'production' ? [purgecss] : []
-        )]
+    plugins: ['postcss-preset-env'],
 }
